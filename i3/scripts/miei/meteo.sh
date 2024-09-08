040e586b9316cfa6e9f0df2a8393724f5538d21c
jifuwater <164806171+jifuwater@users.noreply.github.com>
2024-09-08 19:51:21 +0200

    Delete i3 directory

--- i3/scripts/miei/meteo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-meteo=$(curl wttr.in/Sedriano?format=1| xargs echo)
-first="${meteo%% *}"
-if [ "$meteo" == "" ] || [ "$first" == "Unknown" ]; then
-	echo "  Off" 
-
-else
-	echo $meteo
-fi
